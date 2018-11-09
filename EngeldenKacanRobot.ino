5a2c1f6f725985477d524e1ad782b2b34a718b18
sidarkolak21 <44888400+sidarkolak21@users.noreply.github.com>
2018-11-09 07:53:27 +0300

    Add files via upload

---
+++ EngeldenKacanRobot.ino
@@ -0,0 +1,57 @@
+//Sol Motor
+int in1=6;
+int in2=7;
+//Sag Motor
+int in3=8;
+int in4=9;
+//HCSR04
+int trigPin=4;
+int echoPin=5;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
+  Serial.begin(9600);
+}
+void ileri()
+{
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+}
+void saga()
+{
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sure,mesafe;
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  sure = pulseIn(echoPin,HIGH);
+  mesafe = (sure/2) / 29.1;
+  Serial.println(mesafe);
+  if(mesafe <= 30)
+  {
+    saga();
+  }
+  else
+  {
+    ileri();
+  }
+}
